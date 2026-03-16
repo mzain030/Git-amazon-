f1a270e7ad35858ed326aa0b2bb505c16fe1489b
shahzaib <shahzaibdon43210@gmail.com>
2026-03-16 14:54:50 +0500

    shahzaib ali

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const HomePage = () => {
   return (
     <div>
-      Muhammad Zain Naseer
+      <div><h1>Shahzaib ali</h1></div>
+       Search:<input type="text" />
+      <div>
+      <button type='button'>click me</button>
+      </div>
     </div>
   )
 }
